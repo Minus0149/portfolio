19c66dfb3129b47affcf4cff85046fc5b2f7bd7c
Minus <minus.official.01@gmail.com>
2022-05-02 17:02:20 +0530

    Added some content to index.js

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,16 @@ import Head from "next/head";
 import Image from "next/image";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Head>Minus - portfolio</Head>
+      <div className="h-[100vh] flex flex-col justify-center align-middle text-center">
+        <h1 className="text-[72px] bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 font-extrabold">
+          Start of our project
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
