7f7290743d582c64aa0b940b9492a5ce39c8be65
Minus <minus.official.01@gmail.com>
2022-05-05 15:31:53 +0530

    Added a simple blueprint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
+import Sidebar from "@components/sidebar";
 import "@styles/tailwind.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="grid grid-cols-12 gap-6 my-14 lg:px-48 sm:px-5">
+      <div className="col-span-12 lg:col-span-3 rounded-2xl p-4 text-center">
+        <Sidebar />
+      </div>
+      <div className="col-span-12 lg:col-span-9 rounded-2xl">
+        <Component {...pageProps} />;
+      </div>
+    </div>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,10 @@ import Image from "next/image";
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>Minus - portfolio</Head>
-      <div className="h-[100vh] flex flex-col justify-center align-middle text-center">
-        <h1 className="text-[72px] bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-blue-500 font-extrabold">
-          Start of our project
-        </h1>
-      </div>
+      <Head>
+        <title>Minus - portfolio</title>
+      </Head>
+      <div>Main content</div>
     </div>
   );
 };
